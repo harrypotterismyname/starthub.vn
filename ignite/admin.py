671b9ea4dd58061cc55869fd9fe5b85b3d74556a
_fountainhead_ <smile@edusmile.edu.vn>
2013-08-14 18:01:02 +0700

    admin

---
+++ ignite/admin.py
@@ -0,0 +1,12 @@
+__author__ = 'hongleviet'
+from django.contrib import admin
+from ignite.models import *
+
+
+admin.site.register(Company)
+admin.site.register(Person)
+admin.site.register(TeamMember)
+admin.site.register(Founder)
+admin.site.register(Category)
+
+
