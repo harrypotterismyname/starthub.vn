f7b3ea1fc8764954182a80ff8e62dd1a3f61d6ea
_fountainhead_ <smile@edusmile.edu.vn>
2013-08-14 17:28:00 +0700

    admin

--- startups/urls.py
+++ startups/urls.py
@@ -8,7 +8,7 @@ from ignite.views import *
 urlpatterns = patterns('',
     # Examples:
      url(r'^$', home, name='home'),
-     url(r'^company/(?P<id>\d+)-(.+)/', company)
+     url(r'^company/(?P<id>\d+)-(.+)/', company),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
